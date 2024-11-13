836ea7fd587ef7e2864fa4ab78dbbeaaa8eea540
SpatLyu <lyu.geosocial@gmail.com>
2024-11-13 20:01:40 +0800

    add cisp logo

--- cisp_logo.R
+++ cisp_logo.R
@@ -8,26 +8,26 @@ library(hexSticker)
 library(magick)
 
 sticker(
-  subplot = "./fig_cisp.png",
+  subplot = "./fig_cisp1.png",
   s_x = 1.005,
   s_y = 0.955,
-  s_width = .6,
-  s_height = .6,
-  package = "Spatially Explicit Stratified Power",
+  s_width = .5,
+  s_height = .5,
+  package = "Spatial Pattern Correlation",
   p_family = "ShineTypewriter",
-  p_size = 5.5,
+  p_size = 6.25,
   p_x = 1.00,
-  p_y = 1.60,
+  p_y = 1.65,
   p_color = ggplot2::alpha("#aa3322",1),
   dpi = 300,
   asp = 1,
-  h_size = 2.55,
-  h_color = ggplot2::alpha("#c3c3b4",1),
-  h_fill = '#ffffff',
+  h_size = 1.75,
+  h_color = ggplot2::alpha("#aa3322",1),
+  h_fill = '#f4f1eb',
   white_around_sticker = F,
   url = "https://ausgis.github.io/cisp",
-  u_color = ggplot2::alpha("#3e3221",.75),
-  u_size = 5.45,
+  u_color = ggplot2::alpha("#aa3322",.75),
+  u_size = 5.55,
   filename = "cisp_logo.png"
 )
 
